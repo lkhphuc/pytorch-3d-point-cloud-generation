f9e9a085df0df09b065988d564847c756068b5e4
Phuc Le <liongates011@gmail.com>
2018-11-13 15:52:37 +0700

    format train logic

--- train_stg1.py
+++ train_stg1.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     print("Pretrain structure generator with fixed viewpoints")
     print("=======================================================")
 
-    print("setting configurations...")
+    print("Setting configurations...")
     cfg = options.get_arguments()
 
     EXPERIMENT = f"{cfg.model}_{cfg.experiment}"
@@ -29,38 +29,40 @@ if __name__ == "__main__":
     if not os.path.exists(MODEL_PATH):
         os.makedirs(MODEL_PATH)
 
+    print("Create Dataloader")
+    tfms = transforms.ToTensor()
+    dataloaders = utils.make_data_fixed(cfg, tfms)
+
+    print("Define losses")
+    l1_loss = nn.L1Loss()
+    bce_loss = nn.BCEWithLogitsLoss()
+    criterions = [l1_loss, bce_loss]
+
+    print("Build Structure Generator")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = utils.build_structure_generator(cfg).to(device)
+
+    print("Create optimizer and scheduler")
+    optimizer = utils.make_optimizer(cfg, model)
+    scheduler = utils.make_lr_scheduler(cfg, optimizer)
+
+    print("Create logger")
     logger = logging.getLogger("logger")
     logger.setLevel(logging.DEBUG)
     if not logger.hasHandlers():
         logger.addHandler(
             logging.FileHandler(filename=f"logs/{EXPERIMENT}.log"))
 
+    print("Create tensorboard logger")
     writer = SummaryWriter(comment="_"+EXPERIMENT)
-
-    print("create Dataloader")
-    tfms = transforms.ToTensor()
-    dataloader = utils.make_data_fixed(cfg, tfms)
-
-    print("define losses")
-    l1_loss = nn.L1Loss()
-    bce_loss = nn.BCEWithLogitsLoss()
-    criterions = [l1_loss, bce_loss]
-
-    print("build Structure Generator")
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = utils.build_structure_generator(cfg).to(device)
-
-    print("create optimizer and scheduler")
-    optimizer = utils.make_optimizer(cfg, model)
-    scheduler = utils.make_lr_scheduler(cfg, optimizer)
 
     def on_after_epoch(model, df_hist, images, epoch):
         utils.save_best_model(MODEL_PATH, model, df_hist)
+        utils.log_hist(logger, df_hist)
         utils.write_on_board_losses(writer, df_hist)
         utils.write_on_board_images(writer, images, epoch)
-        utils.log_hist(logger, df_hist)
 
-    trainer = Trainer_stg1(cfg, dataloader, criterions, device, on_after_epoch)
+    trainer = Trainer_stg1(cfg, dataloaders, criterions, device, on_after_epoch)
 
     hist = trainer.train(model, optimizer, scheduler)
     hist.to_csv(f"{EXPERIMENT}/hist.csv", index=False)
